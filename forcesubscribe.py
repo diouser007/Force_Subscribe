1d71f83fb02ee0b8befb4ed5c0ff5017780b5d2b
diouser007 <71636013+diouser007@users.noreply.github.com>
2024-08-02 18:22:24 +0530

    Update forcesubscribe.py

--- forcesubscribe.py
+++ forcesubscribe.py
@@ -9,7 +9,7 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1001838119618').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 
 # force subscribe main code :-
